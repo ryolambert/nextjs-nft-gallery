19c0a1d80d3139eb79230417003064cae48d65f3
Ryo Lambert <19197856+ryolambert@users.noreply.github.com>
2023-02-03 16:12:28 -0700

    FIX: Add remote sources for SSR NFT images

--- next.config.js
+++ next.config.js
@@ -10,6 +10,19 @@ module.exports = withImages({
 		// @see https://github.com/TanStack/query/issues/1458#issuecomment-1022396964
 		swcPlugins: [['next-superjson-plugin', {}]]
 	},
+	images: {
+		domains: ['nft-cdn.alchemy.com'],
+		formats: ['image/webp'],
+		minimumCacheTTL: 600,
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+				pathname: '/alchemyapi/image/upload/thumbnail/**'
+			},
+			{ protocol: 'https', hostname: 'ipfs.io', pathname: '/ipfs/**' }
+		]
+	},
 	poweredByHeader: false,
 	trailingSlash: true,
 	reactStrictMode: true,
