838998d7118dece8db0942f3d383ec929c7753b5
Ryo Lambert <19197856+ryolambert@users.noreply.github.com>
2023-02-03 15:58:03 -0700

    FIX: Resolve RQ SSR bug for prefetchInfiniteQuery

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ module.exports = withImages({
 	eslint: {
 		dirs: ['.']
 	},
+	experimental: {
+		// Resolves Next.js SSR bug for react-query prefetchInfiniteQuery
+		// @see https://github.com/TanStack/query/issues/1458#issuecomment-1022396964
+		swcPlugins: [['next-superjson-plugin', {}]]
+	},
 	poweredByHeader: false,
 	trailingSlash: true,
 	reactStrictMode: true,
